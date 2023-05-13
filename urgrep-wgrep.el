3d3ea6fdfb2325d09aa292237bf3da62d352a01f
Jim Porter <jporterbugs@gmail.com>
2023-05-13 12:34:27 -0700

    Update version to 0.1.1-git

--- urgrep-wgrep.el
+++ urgrep-wgrep.el
@@ -4,9 +4,9 @@
 
 ;; Author: Jim Porter
 ;; URL: https://github.com/jimporter/urgrep
-;; Version: 0.1.0
+;; Version: 0.1.1-git
 ;; Keywords: grep, search
-;; Package-Requires: ((urgrep "0.1.0") (wgrep "2.3.4"))
+;; Package-Requires: ((urgrep "0.1.1-git") (wgrep "2.3.4"))
 
 ;; This file is NOT part of GNU Emacs.
 
