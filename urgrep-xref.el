a1d6ae39e9717e085ba624559d0fcccc2dca9a3f
Jim Porter <jporterbugs@gmail.com>
2024-08-22 21:48:34 -0700

    Update version elsewhere too

--- urgrep-xref.el
+++ urgrep-xref.el
@@ -4,7 +4,7 @@
 
 ;; Author: Jim Porter
 ;; URL: https://github.com/jimporter/urgrep
-;; Version: 0.5.1-git
+;; Version: 0.5.2-git
 ;; Keywords: grep, search
 
 ;; This file is NOT part of GNU Emacs.
